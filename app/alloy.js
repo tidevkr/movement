f8e20e63ac051202287fd62aa6bd48ebc0c292c8
Jong Eun Lee <yomybaby@yomybaby.com>
2014-07-22 11:26:40 +0900

    add settings model

--- app/alloy.js
+++ app/alloy.js
@@ -9,3 +9,11 @@
 // object. For example:
 //
 // Alloy.Globals.someGlobalFunction = function(){};
+
+
+// 앱이 종료후에도 기억해야하는 앱 관련 세팅정보를 저장하는 Model
+// 필요한 곳에서 코드 한줄로 불러와서 사용하면 됩니다.(Singleton)
+// initialize할때 fetch하므로 따로 fetch할 필요가 없다.
+var settings = Alloy.Models.instance('settings');
+Ti.API.info(settings.get('test'));
+// settings.save('test',100);
---
+++ app/models/settings.js
@@ -0,0 +1,43 @@
+var count = 0;
+exports.definition = {
+	config: {
+		adapter: {
+			type: "properties",
+			collection_name: "settings"
+		}
+	},
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+			defaults : {
+				id : 'staticSettings' //id를 고정하여 id 지정 없이 singleton을 사용 가능하게 함
+			},
+			initialize : function(){
+				this.fetch();
+			}
+			// extended functions and properties go here
+		});
+
+		return Model;
+	},
+	extendCollection: function(Collection) {
+		_.extend(Collection.prototype, {
+			// extended functions and properties go here
+			 /**
+		     * clean up any models from the properties db
+		     */
+		    cleanup : function() {
+		        var regex = new RegExp("^(" + this.config.adapter.collection_name + ")\\-(.+)$");
+		        var TAP = Ti.App.Properties;
+		        _.each(TAP.listProperties(), function(prop) {
+		            var match = prop.match(regex);
+		            if (match) {
+		                TAP.removeProperty(prop);
+		                Ti.API.info('deleting old model ' + prop);
+		            }
+		        });
+		    }
+		});
+
+		return Collection;
+	}
+};
